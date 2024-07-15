434b3e9a0c33b14ad0fb003fac3d5839bb5e1c79
matéo! <6matbub@gmail.com>
2024-07-15 08:42:29 -0700

    STL-2: format code

--- gulpfile.js
+++ gulpfile.js
@@ -19,103 +19,112 @@ const nestingPlugin = require('postcss-nesting');
 const customMediaPlugin = require('postcss-custom-media');
 const concat = require('gulp-concat');
 function buildStyles() {
-	// convert CSS variables to static values for older browsers
-	const cssVariablesConfig = cssVariablesPlugin({
-		variables: {
-			'--theme-primary': '#FFD600',
-			'--base-black': '#000000',
-			'--base-white': '#FFFFFF',
-			'--text-primary': '#FFFFFF',
-			'--text-secondary': '#a2a2a2',
-		}
-	});
-	const cssMixinsConfig = cssMixinsPlugin({});
-	const calcConfig = calcPlugin({/* handle pixel fall back for rem values */ });
-	const nestingConfig = nestingPlugin({});
-	const customMediaConfig = customMediaPlugin({});
+  // convert CSS variables to static values for older browsers
+  const cssVariablesConfig = cssVariablesPlugin({
+    variables: {
+      '--theme-primary': '#FFD600',
+      '--base-black': '#000000',
+      '--base-white': '#FFFFFF',
+      '--text-primary': '#FFFFFF',
+      '--text-secondary': '#a2a2a2'
+    }
+  });
+  const cssMixinsConfig = cssMixinsPlugin({});
+  const calcConfig = calcPlugin({
+    /* handle pixel fall back for rem values */
+  });
+  const nestingConfig = nestingPlugin({});
+  const customMediaConfig = customMediaPlugin({});
 
-	return gulp.src('src/*.css')
-		.pipe(sourcemapsPlugin.init())
-		.pipe(postcssPlugin([
-			autoprefixerPlugin,
-			cssVariablesConfig,
-			cssMixinsConfig,
-			calcConfig,
-			nestingConfig,
-			customMediaConfig
-		]))
-		.pipe(sourcemapsPlugin.write('./maps'))
-		.pipe(gulp.dest('dist'));
+  return gulp
+    .src('src/*.css')
+    .pipe(sourcemapsPlugin.init())
+    .pipe(
+      postcssPlugin([
+        autoprefixerPlugin,
+        cssVariablesConfig,
+        cssMixinsConfig,
+        calcConfig,
+        nestingConfig,
+        customMediaConfig
+      ])
+    )
+    .pipe(sourcemapsPlugin.write('./maps'))
+    .pipe(gulp.dest('dist'));
 }
 
 /**
  * @name minifyCSS - npm run minify
- * 
+ *
  * @readme
  * Minifies CSS files and generates source maps.
- * 
+ *
  * This function takes CSS files from the 'dist' directory, minifies them,
  * generates source maps for debugging, and saves the output back to the 'dist' directory.
  */
 function minifyCSS() {
-	return gulp.src('dist/*.css')
-		.pipe(sourcemapsPlugin.init({ loadMaps: true }))
-		.pipe(postcssPlugin([cssnanoPlugin]))
-		.pipe(renamePlugin({ suffix: '.min' }))
-		.pipe(sourcemapsPlugin.write('/maps'))
-		.pipe(gulp.dest('dist'));
+  return gulp
+    .src('dist/*.css')
+    .pipe(sourcemapsPlugin.init({ loadMaps: true }))
+    .pipe(postcssPlugin([cssnanoPlugin]))
+    .pipe(renamePlugin({ suffix: '.min' }))
+    .pipe(sourcemapsPlugin.write('/maps'))
+    .pipe(gulp.dest('dist'));
 }
 
 function watch() {
-	const watcher = gulp.watch('src/*.css', build)
-	watcher.on('change', function (fileName) {
-		console.log('Rebuilding ' + fileName)
-	});
+  const watcher = gulp.watch('src/*.css', build);
+  watcher.on('change', function (fileName) {
+    console.log('Rebuilding ' + fileName);
+  });
 }
 
 function lintDevCode() {
-	const stylelintRules = {
-		"color-no-invalid-hex": 2,
-	}
+  const stylelintRules = {
+    'color-no-invalid-hex': 2
+  };
 
-	const stylelintConfig = stylelintPlugin({ rules: stylelintRules })
+  const stylelintConfig = stylelintPlugin({ rules: stylelintRules });
 
-	const postcssOptions = [
-		stylelintConfig,
-		reporterPlugin({ clearMessages: true })
-	]
+  const postcssOptions = [
+    stylelintConfig,
+    reporterPlugin({ clearMessages: true })
+  ];
 
-	return gulp.src('src/*.css')
-		.pipe(postcssPlugin(postcssOptions))
+  return gulp.src('src/*.css').pipe(postcssPlugin(postcssOptions));
 }
 
 /** @deprecated I'm not supporting this, but it should work out of the box */
 function buildSass() {
-	const sassConfig = sassPlugin({
-		outputStyle: 'compressed'
-	})
-	const sassHandler = sassConfig.on('error', sassPlugin.logError);
-	return gulp.src('src/*.scss')
-		.pipe(sassHandler)
-		.pipe(gulp.dest('src/'))
+  const sassConfig = sassPlugin({
+    outputStyle: 'compressed'
+  });
+  const sassHandler = sassConfig.on('error', sassPlugin.logError);
+  return gulp.src('src/*.scss').pipe(sassHandler).pipe(gulp.dest('src/'));
 }
 
 function combineStyles() {
-	return gulp.src('dist/*.css') // Get all individual stylesheets from dist
-		.pipe(concat('chroniconl-stylist.css')) // Combine them into one file
-		.pipe(gulp.dest('dist')); // Write the combined file to dist
+  return gulp
+    .src('dist/*.css') // Get all individual stylesheets from dist
+    .pipe(concat('chroniconl-stylist.css')) // Combine them into one file
+    .pipe(gulp.dest('dist')); // Write the combined file to dist
 }
 
 /**
  * @name build - npm run build
- * @readme a series of tasks to build the CSS files 
- * 
+ * @readme a series of tasks to build the CSS files
+ *
  * if you don't like or want a particular task, you can remove it from the series
- * 
+ *
  * @note if you want to use scss files, you can use the `buildSass` task at your own risk
  * it would need to be treated as a preprocess step before the `buildStyles` task
  */
-const build = gulp.series(/*buildSass,*/ buildStyles, lintDevCode, minifyCSS, combineStyles /*...*/);
+const build = gulp.series(
+  /*buildSass,*/ buildStyles,
+  lintDevCode,
+  minifyCSS,
+  combineStyles /*...*/
+);
 
 gulp.task('build-sass', buildSass);
 gulp.task('build-styles', buildStyles);
